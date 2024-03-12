8e25ce7c3f78ede33102baa29e8de7ec6a6579cb
asadcodedistrict <asad.hafeez@codedistrict.com>
2024-03-12 14:16:54 +0500

    Login_page

--- Pages/login_page.py
+++ Pages/login_page.py
@@ -8,13 +8,16 @@ class LoginPage:
     def __init__(self, driver):
         self.driver = driver
 
+    def wait_for_element_visibility(self, locator):
+        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator))
+
     def enter_email(self, email):
         email_field = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located( loginLocators.email_textbox))
         email_field.send_keys(email)
         
 
     def enter_password(self, password):
-        password_field = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(loginLocators.password_textbox))
+        password_field = WebDriverWait(self.driver, 15).until(EC.visibility_of_element_located(loginLocators.password_textbox))
         password_field.send_keys(password)
 
     def click_login(self):
@@ -32,3 +35,16 @@ class LoginPage:
         self.enter_password(password)
         self.click_login()
         time.sleep(3)
+
+    def login_with_empty_fields(self,):
+        self.click_login()
+        time.sleep(3)
+        email_error_message = self.wait_for_element_visibility(loginLocators.empty_email_field_error)
+        print(email_error_message.text)
+        expected_error_message = "Please enter your email."
+        assert email_error_message.text == expected_error_message, f"Unexpected email error message: Expected '{expected_error_message}', Found '{email_error_message.text}'"
+
+        time.sleep(3)
+        password_error_message = self.wait_for_element_visibility(loginLocators.empty_password_field_error)
+        assert password_error_message.text == "Please enter password", f"Unexpected password error message: {password_error_message.text}"
+        time.sleep(1)
