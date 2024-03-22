d4004804e279201f84070f4f3036a7be40678eac
umerehsan418 <146002089+umerehsan418@users.noreply.github.com>
2024-03-22 14:08:53 +0500

    added comments in code

--- Pages/registration/payment_page.py
+++ Pages/registration/payment_page.py
@@ -10,20 +10,22 @@ class payment_processing:
     def __init__(self, driver):
         self.driver = driver
     def payment_processing(self):
+        # waiting for element to visible.
         continue_button = WebDriverWait(self.driver, 40).until(EC.element_to_be_clickable((By.XPATH, "(//button[normalize-space()='Continue'])[1]")))
+        # switching into iframes of card numer
         WebDriverWait(self.driver, 30).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, "//iframe[@id='braintree-hosted-field-number']")))
         WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//input[@class='number' and @id='credit-card-number']"))).send_keys("5555555555554444")
+        # switching back to main frame
         self.driver.switch_to.default_content()
 
         WebDriverWait(self.driver, 30).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, "//iframe[@id='braintree-hosted-field-expirationDate']")))
         WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "(//input[@id='expiration'])[1]"))).send_keys("11/2027")
 
         self.driver.switch_to.default_content()
-
+        # switching into iframes of expiry date
         WebDriverWait(self.driver, 30).until(EC.frame_to_be_available_and_switch_to_it((By.XPATH, "//iframe[@id='braintree-hosted-field-cvv']")))
         WebDriverWait(self.driver, 30).until(EC.element_to_be_clickable((By.XPATH, "//input[@class='cvv' and @id='cvv']"))).send_keys("456")
+        # switching back to main frame
         self.driver.switch_to.default_content()
-        time.sleep(3)
         continue_button.click()
-        time.sleep(5)
 
