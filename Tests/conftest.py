b0ef53094195277749403ea4a6030852c10a1510
umerehsan418 <umer.ehsan@codedistrict.com>
2024-09-04 16:41:44 +0500

    changes

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,9 +1,12 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture(scope="module")
 def driver_setup(request):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=chrome")
     driver = webdriver.Chrome()
     request.addfinalizer(driver.quit)
     return driver
