5198cb33cbdc01d9a519a05ab99beab9ea3f49fb
Asad hafeez <asad.hafeez@codedistrict.com>
2024-09-09 11:27:52 +0000

    conftest.py edited online with Bitbucket

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -1,11 +1,15 @@
 import pytest
 from selenium import webdriver
 
-
 @pytest.fixture(scope="module")
 def driver_setup(request):
-    options = Options()
     options = webdriver.ChromeOptions()
     options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
+
+    # Teardown after tests
+    def teardown():
+        driver.quit()
+    request.addfinalizer(teardown)
+
     return driver
