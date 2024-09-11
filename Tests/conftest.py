8c2869960d32f6abf5751fc8d2922ba7251f26a1
asadcodedistrict <asad.hafeez@codedistrict.com>
2024-09-11 19:15:49 +0500

    testing

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -3,13 +3,6 @@ from selenium import webdriver
 
 @pytest.fixture(scope="module")
 def driver_setup(request):
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    driver = webdriver.Chrome(options=options)
-
-    # Teardown after tests
-    def teardown():
-        driver.quit()
-    request.addfinalizer(teardown)
-
+    driver = webdriver.Chrome()
+    request.addfinalizer(driver.quit)
     return driver
