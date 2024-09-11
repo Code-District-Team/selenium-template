fe0d416226235821d7a0a76935110e3808163de4
asadcodedistrict <asad.hafeez@codedistrict.com>
2024-09-11 19:18:50 +0500

    fixing

--- Resources/loginData.py
+++ Resources/loginData.py
@@ -1,29 +1,10 @@
 class loginTestData:
-    invalid_credentials = {
-        "InvalidEmail": "test23@yopmail.com",
-        "InvalidPassword": "test123@"
-        
-    }
-    valid_credential = {
-        "validEmail" : "asad.hafeez@codedistrict.com",
-        "password" : "123456@Aa"
 
-    }
-    valid_credential_individual_to_freelance = {
-        "validEmail": "asad.hafeez+71@codedistrict.com",
+
+    valid_credential = {
+        "validEmail": "asad.hafeez@codedistrict.com",
         "password": "123456@Aa"
-    }
-    valid_credential_individual_to_business_basic = {
-        "validEmail": "asad.hafeez+72@codedistrict.com",
-        "password": "123456@Aa"
-    }
-    valid_credential_individual_to_business_plus = {
-        "validEmail": "asad.hafeez+73@codedistrict.com",
-        "password": "123456@Aa"
-    }
-    valid_credential_busniess_basic_to_business_plus = {
-        "validEmail": "asad.hafeez+74@codedistrict.com",
-        "password": "123456@Aa"
+
     }
 
    
